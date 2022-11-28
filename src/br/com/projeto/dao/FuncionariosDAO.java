b438ed59a3aab75ba5f0b2fe84af187b5fcb365e
Yuri Gomes <yuri.gcandido@hotmail.com>
2022-11-28 19:19:50 -0300

    Incluindo o método alterarFuncionario e excluirFuncionario

--- src/br/com/projeto/dao/FuncionariosDAO.java
+++ src/br/com/projeto/dao/FuncionariosDAO.java
@@ -71,7 +71,82 @@ public class FuncionariosDAO {
             JOptionPane.showMessageDialog(null,"Erro" + erro);
         }
         
-    }  
+    }
+    
+    // método alterarFuncionario
+    
+        public void alterarFuncionarios(Funcionarios obj) {
+            
+            try {
+            
+            String sql = "update tb_funcionarios set nome=?,rg=?,cpf=?,email=?, senha=?, cargo=?, nivel_acesso =?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+                   
+            
+            //PreparedStatement stmt =(PreparedStatement) con.PreparedStatement(sql);
+            PreparedStatement stmt = con.prepareStatement(sql);
+            
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getSenha());
+            stmt.setString(6, obj.getCargo());
+            stmt.setString(7, obj.getNivel_acesso());
+            stmt.setString(8, obj.getTelefone());
+            stmt.setString(9, obj.getCelular());
+            stmt.setString(10, obj.getCep());
+            stmt.setString(11, obj.getEndereco());
+            stmt.setInt(12, obj.getNumero());
+            stmt.setString(13, obj.getComplemento());
+            stmt.setString(14, obj.getBairro());
+            stmt.setString(15, obj.getCidade());
+            stmt.setString(16, obj.getUf());
+            stmt.setInt(17, obj.getId());
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null,"Alterado com sucesso!");
+            
+            
+            
+            
+        } catch(Exception erro) {
+            JOptionPane.showMessageDialog(null,"Erro" + erro);
+        }
+        
+    }    
+    
+    // metodo excluirFuncionario
+    
+    
+    public void excluirFuncionarios(Funcionarios obj) {
+            
+            try {
+            
+            String sql = "delete from tb_funcionarios where id=?";
+            
+            //PreparedStatement stmt =(PreparedStatement) con.PreparedStatement(sql);
+            PreparedStatement stmt = con.prepareStatement(sql);
+           
+            stmt.setInt(1, obj.getId());
+           
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null,"Excluído com sucesso!");
+            
+            
+            
+            
+        } catch(Exception erro) {
+            JOptionPane.showMessageDialog(null,"Erro" + erro);
+            }
+        
+        }   
+    
+    
     
     // metodo listar funcionarios
     public List<Funcionarios> listarFuncionarios(){
